9cee6a08421bad02814a69600db798c6e718f25f
Diego Alonso Cárcamo Gutiérrez <dcarcamog@fpcoruna.afundacion.org>
2022-09-27 09:14:19 +0200

    Primer ejercicio Python

---
+++ sprint0python/concurso.py
@@ -0,0 +1,10 @@
+print ("¿Cuánto es 1+1?")
+print ("-Opción a: 1")
+print ("-Opción b: 2")
+print ("-Opción c: 3")
+print ("Escoja una opción: ")
+a=input()
+if a != "b":
+    print("Falso")
+else:
+    print("Verdadero")
