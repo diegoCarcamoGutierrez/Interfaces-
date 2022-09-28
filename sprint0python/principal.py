3378cffee1f9e9d9e19358ecf779d250e2921f02
Diego Alonso Cárcamo Gutiérrez <dcarcamog@fpcoruna.afundacion.org>
2022-09-28 09:58:33 +0200

    Ejercicio 5 completado

--- sprint0python/principal.py
+++ sprint0python/principal.py
@@ -1,4 +1,18 @@
+import time
+from re import A
+from fibbonaci import funcion_fibbonaci
 from fibbonaci2 import funcion_fibbonaci2
 
 n=int(input("Introduce un número entero"))
-funcion_fibbonaci2(n)
+
+
+print("Escoja una forma para calcular fibbonaci")
+print("a-primer algoritmo")
+print("b-segundo algoritmo")
+opc=input()
+if opc=='a':
+    start_time=start_time()
+    funcion_fibbonaci(n)
+    end_time=
+elif opc=='b':
+    funcion_fibbonaci2(n)
