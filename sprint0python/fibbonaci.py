12f3f61e1b7d94fa994ad63a6f0d0e612150f128
Diego Alonso Cárcamo Gutiérrez <dcarcamog@fpcoruna.afundacion.org>
2022-09-28 09:14:14 +0200

    Ejercicio 3 completado

---
+++ sprint0python/fibbonaci.py
@@ -0,0 +1,12 @@
+def funcion_fibbonaci(n):
+    resu=0
+    o=2
+    i=0
+    j=1
+
+    while o<=n:
+        resu=i+j
+        i=j
+        j=resu
+        o=o+1
+    print (str(resu))
---
+++ sprint0python/principal.py
@@ -0,0 +1,3 @@
+from fibbonaci import funcion_fibbonaci
+n=int(input("Introduce un número entero"))
+funcion_fibbonaci(n)
