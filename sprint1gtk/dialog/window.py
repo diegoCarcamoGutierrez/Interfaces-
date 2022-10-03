870394c7a0bb6d591d3d26d6f8a35dd9a70527cb
Diego Alonso Cárcamo Gutiérrez <dcarcamog@fpcoruna.afundacion.org>
2022-10-03 17:19:38 +0200

    Ejercicio 2 completado. 'Unable to find python module'

---
+++ sprint1gtk/dialog/window.py
@@ -0,0 +1,18 @@
+from cProfile import label
+from tkinter import Button
+from turtle import title
+import gi
+gi.require_version("Gtk","3.0")
+from gi.repository import Gtk
+
+class MainWindow(Gtk.Window):
+    button=Gtk.Button(label="Realizar acción")
+
+    def __init__(self):
+        super().__init__(title="Main")
+        self.connect("destroy",Gtk.main_quit)
+        self.button.connect("clicked",self.on_button_clicked)
+        self.add(self.button)
+
+    def on_button_clicked(self,widget):
+        pass
