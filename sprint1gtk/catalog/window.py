ea84d897ed848f6076186d905a02cc6b89e0afc9
Diego Alonso Cárcamo Gutiérrez <dcarcamog@fpcoruna.afundacion.org>
2022-10-05 11:16:10 +0200

    Ejercicio 6 completado

--- sprint1gtk/catalog/window.py
+++ sprint1gtk/catalog/window.py
@@ -1,4 +1,5 @@
 import gi
+from gi.repository import GdkPixbuf
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
@@ -24,12 +25,26 @@ class MainWindow(Gtk.Window):
         scrolled.set_policy(Gtk.PolicyType.NEVER,Gtk.PolicyType.AUTOMATIC)
         scrolled.add(self.flowbox)
         self.add(scrolled)
-
-        cell_one = cell.Cell("Monado REX+", Gtk.Image.new_from_file("data/edited/MonadoREX+.jpg"))
-        cell_two = cell.Cell("Bola Smash", Gtk.Image.new_from_file("data/edited/bolaSmash.jpg"))
-        cell_three = cell.Cell("Cello", Gtk.Image.new_from_file("data/edited/cello.jpg"))
-        cell_four = cell.Cell("Oreo", Gtk.Image.new_from_file("data/edited/oureol.jpg"))
-        cell_five = cell.Cell("Empanadah", Gtk.Image.new_from_file("data/edited/empanadah.jpg"))
+        image1 = Gtk.Image()
+        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/MonadoREX+.jpg", 200, 200, False)
+        image1.set_from_pixbuf(pixbuf)
+        image2 = Gtk.Image()
+        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/bolaSmash.jpg", 200, 200, False)
+        image2.set_from_pixbuf(pixbuf)
+        image3 = Gtk.Image()
+        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/cello.jpg", 200, 200, False)
+        image3.set_from_pixbuf(pixbuf)
+        image4 = Gtk.Image()
+        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/oureol.jpg", 200, 200, False)
+        image4.set_from_pixbuf(pixbuf)
+        image5 = Gtk.Image()
+        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/empanadah.jpg", 200, 200, False)
+        image5.set_from_pixbuf(pixbuf)
+        cell_one = cell.Cell("Monado REX+", image1)
+        cell_two = cell.Cell("Bola Smash", image2)
+        cell_three = cell.Cell("Cello",image3)
+        cell_four = cell.Cell("Oreo", image4)
+        cell_five = cell.Cell("Empanadah", image5)
         self.flowbox.add(cell_one)
         self.flowbox.add(cell_two)
         self.flowbox.add(cell_three)
