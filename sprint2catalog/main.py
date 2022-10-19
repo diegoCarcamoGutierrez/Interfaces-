7b1f698d321979aea581ae49411d3ceb0661767d
Diego Alonso Cárcamo Gutiérrez <dcarcamog@fpcoruna.afundacion.org>
2022-10-19 09:07:31 +0200

    Ejercicio 2 completado

--- sprint2catalog/main.py
+++ sprint2catalog/main.py
@@ -2,10 +2,11 @@ import gi
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
-from window import MainWindow
+import window
+from load_window import LoadWindow
 
-#Mostrar la ventana principal
-win = MainWindow()
+# Mostrar la ventana principal
+win = LoadWindow()
 win.show_all()
 
 Gtk.main()
