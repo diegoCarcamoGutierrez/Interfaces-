c443cea7078117948420cef8d16c582a4c53031f
Diego Alonso Cárcamo Gutiérrez <dcarcamog@fpcoruna.afundacion.org>
2022-10-19 10:38:51 +0200

    Ejercicio 3 completado

--- sprint2catalog/load_window.py
+++ sprint2catalog/load_window.py
@@ -1,7 +1,9 @@
 import gi
+import requests, threading, shutil
 
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
+from gi.repository import Gtk, GLib
+from window import MainWindow
 
 
 class LoadWindow(Gtk.Window):
@@ -9,6 +11,7 @@ class LoadWindow(Gtk.Window):
     spinner = Gtk.Spinner()
     box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=20)
 
+    # definición del método constructor
     def __init__(self):
         super().__init__(title="")
         self.connect("destroy", Gtk.main_quit)
@@ -19,3 +22,31 @@ class LoadWindow(Gtk.Window):
         self.box.pack_start(self.label, False, False, 0)
         self.box.pack_start(self.spinner, False, False, 0)
         self.add(self.box)
+        self.launch_load()
+
+    def launch_load(self):
+        thread = threading.Thread(target=self.load_json(), args=())
+        thread.start()
+
+    def load_json(self):
+        response = requests.get("https://raw.githubusercontent.com/diegoCarcamoGutierrez/Interfaces-/main/catalog.json")
+        json_list = response.json()
+
+        result = []
+
+        for json_item in json_list:
+            name = json_item.get("name")
+            description = json_item.get("description")
+            image_url = json_item.get("image_url")
+            r = requests.get(image_url, stream=True)
+            with open("temp.jpg", 'wb') as f:
+                shutil.copyfileobj(r.raw, f)
+            image = Gtk.Image.new_from_file("temp.jpg")
+            result.append({"name": name, "description": description, "gtk_image": image})
+        GLib.idle_add(self.start_main_window, result)
+
+    def start_main_window(self, loaded_items_list):
+        win = MainWindow(loaded_items_list)
+        win.show_all()
+        self.disconnect_by_func(Gtk.main_quit)
+        self.close()
--- sprint2catalog/window.py
+++ sprint2catalog/window.py
@@ -3,13 +3,13 @@ from gi.repository import GdkPixbuf
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
-import cell
+from cell import Cell
 
 
 class MainWindow(Gtk.Window):
     flowbox = Gtk.FlowBox()
 
-    def __init__(self):
+    def __init__(self, data_source):
         super().__init__(title="imágenes")
         self.connect("destroy", Gtk.main_quit)
         self.set_border_width(15)
@@ -24,29 +24,7 @@ class MainWindow(Gtk.Window):
         scrolled.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
         scrolled.add(self.flowbox)
         self.add(scrolled)
-        # Se cargan las imágenes en los objetos correspondientes
-        image1 = Gtk.Image()
-        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/MonadoREX+.jpg", 200, 200, False)
-        image1.set_from_pixbuf(pixbuf)
-        image2 = Gtk.Image()
-        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/bolaSmash.jpg", 200, 200, False)
-        image2.set_from_pixbuf(pixbuf)
-        image3 = Gtk.Image()
-        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/cello.jpg", 200, 200, False)
-        image3.set_from_pixbuf(pixbuf)
-        image4 = Gtk.Image()
-        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/oureol.jpg", 200, 200, False)
-        image4.set_from_pixbuf(pixbuf)
-        image5 = Gtk.Image()
-        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/empanadah.jpg", 200, 200, False)
-        image5.set_from_pixbuf(pixbuf)
-        cell_one = cell.Cell("Monado REX+", image1)
-        cell_two = cell.Cell("Bola Smash", image2)
-        cell_three = cell.Cell("Cello", image3)
-        cell_four = cell.Cell("Oreo", image4)
-        cell_five = cell.Cell("Empanadah", image5)
-        self.flowbox.add(cell_one)
-        self.flowbox.add(cell_two)
-        self.flowbox.add(cell_three)
-        self.flowbox.add(cell_four)
-        self.flowbox.add(cell_five)
+
+        for item in data_source:
+            cell = Cell(item.get("name"), item.get("gtk_image"))
+            self.flowbox.add(cell)
